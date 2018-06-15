de687ea661df4d3f6f44f8ca72eac02f3be5a75e
rossdaly@gmail.com daly <rossdaly@gmail.com>
2018-06-15 17:07:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,5 +64,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber)
+  if (cardNumber===false) {
+    return 'Sorry, we don't have a credit card on file for you.'
+  }
 }
