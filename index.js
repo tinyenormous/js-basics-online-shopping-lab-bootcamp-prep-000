cab73d5a520f3616c7b4ef9ba765cfe54b96b2ce
rossdaly@gmail.com daly <rossdaly@gmail.com>
2018-06-15 17:11:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function placeOrder(cardNumber) {
   if (cardNumber===undefined) {
     return "Sorry, we don't have a credit card on file for you."
   }
-  printString=`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+  var printString=`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   cart=[]
   return printString
 }
